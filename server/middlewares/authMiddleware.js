18d95fd4bdb14c3f33a1f6ded8fc16d1e0bdf138
shubhampathak <shubham.pathak926@gmail.com>
2023-07-14 12:09:21 +0530

    apply admin middleware and protected routes

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 import jwt from "jsonwebtoken";
+import userModel from "../models/userModel.js";
 
 export const Validation = async (req, res, next) => {
   try {
@@ -6,9 +7,30 @@ export const Validation = async (req, res, next) => {
       req.headers.authorization,
       process.env.JWT_SECRET_KEY
     );
-
+    req.user = decode;
     next();
   } catch (error) {
     console.log(error);
+  }
+};
+
+export const isAdmin = async (req, res, next) => {
+  try {
+    const user = await userModel.findById(req.user._id);
+    if (user.role !== 1) {
+      return res.status(401).send({
+        success: false,
+        message: "UnAuthorised Access",
+      });
+    } else {
+      next();
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(401).send({
+      success: false,
+      message: "Error in Admin Middleware",
+      error,
+    });
   }
 };
--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -4,11 +4,11 @@ import {
   registerController,
   validationController,
 } from "../controllers/authController.js";
-import { Validation } from "../middlewares/authMiddleware.js";
+import { Validation, isAdmin } from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerController);
 router.post("/login", loginController);
-router.get("/validate", Validation, validationController);
+router.get("/validate", Validation, isAdmin, validationController);
 export default router;
