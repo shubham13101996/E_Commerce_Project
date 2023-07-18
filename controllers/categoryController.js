8345af00f27316f525e13bc1f4676c81640b6513
shubhampathak <shubham.pathak926@gmail.com>
2023-07-18 13:11:29 +0530

    create delete api for deleting the category from the database

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -95,3 +95,22 @@ export const singleCategoryController = async (req, res) => {
     });
   }
 };
+
+export const deleteCategoryController = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    await categoryModel.findByIdAndDelete(id);
+    res.status(200).send({
+      success: true,
+      message: "Category Deleted Successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error while deleting the category",
+    });
+  }
+};
--- routes/categoryRoutes.js
+++ routes/categoryRoutes.js
@@ -3,6 +3,7 @@ import { Validation, isAdmin } from "../middlewares/authMiddleware.js";
 import {
   categoryController,
   createCategoryController,
+  deleteCategoryController,
   singleCategoryController,
   updateCategoryController,
 } from "../controllers/categoryController.js";
@@ -21,5 +22,13 @@ router.get("/get-category", categoryController);
 
 // single category list
 router.get("/single-category/:slug", singleCategoryController);
+
+// delete category
+router.delete(
+  "/delete-category/:id",
+  Validation,
+  isAdmin,
+  deleteCategoryController
+);
 
 export default router;
