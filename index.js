a5882a9105ba985407891feaed5a6943c6b30651
shubhampathak <shubham.pathak926@gmail.com>
2023-08-02 14:33:22 +0530

    production requisite

--- index.js
+++ index.js
@@ -6,26 +6,26 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-import path from 'path'
+// import path from 'path'
 const app = express();
 dotenv.config();
-connectDB();
+connectDB();  
 
 // middleware 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan("dev"));
-app.use(express.static(path.join(__dirname,'./client/build')))
+// app.use(express.static(path.join(__dirname,'./client/build')))
 
 // routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.use('*',function(req,res){
-res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
+// app.use('*',function(req,res){
+// res.sendFile(path.join(__dirname,"./client/build/index.html"))
+// })
 
 app.listen(process.env.PORT, () => {
   console.log(
