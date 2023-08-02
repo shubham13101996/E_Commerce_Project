14813e6bd7dbd32bf0c7e18419c0e8d5b55f0e1d
shubhampathak <shubham.pathak926@gmail.com>
2023-08-02 13:57:16 +0530

    prepare build for client and remove nodemon path

--- index.js
+++ index.js
@@ -6,22 +6,26 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
+import path from 'path'
 const app = express();
 dotenv.config();
 connectDB();
 
+// middleware 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan("dev"));
-app.use(cors());
+app.use(express.static(path.join(__dirname,'./client/build')))
 
+// routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.get("/", (req, res) => {
-  return res.send("<h1>WELCOME TO THE E_COMMERCE APP</h1>");
-});
+app.use('*',function(req,res){
+res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(
