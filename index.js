6a42f5956a63f4ebf9c875db5ead932b9c9d7295
shubhampathak <shubham.pathak926@gmail.com>
2023-08-02 15:30:10 +0530

    esmodule fix for __dirname

--- index.js
+++ index.js
@@ -6,32 +6,38 @@ import authRoutes from "./routes/authRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
-// import path from 'path'
-const app = express();
+import path from 'path'
+import { fileURLToPath } from "url";
+
 dotenv.config();
 connectDB();
 
+// esmodule fix 
+const __Filename = fileURLToPath(import.meta.url);
+const __dirname=path.dirname(__Filename)
+
+const app = express();
 // middleware
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(morgan("dev"));
-// app.use(express.static(path.join(__dirname,'./client/build')))
+app.use(express.static(path.join(__dirname,'./client/build')))
 
 // routes
 
-app.get("/", (req, res) => {
-  res.send({
-    message: "Your App is running",
-  });
-});
+// app.get("/", (req, res) => {
+//   res.send({
+//     message: "Your App is running",
+//   });
+// });
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-// app.use('*',function(req,res){
-// res.sendFile(path.join(__dirname,"./client/build/index.html"))
-// })
+app.use('*',function(req,res){
+res.sendFile(path.join(__dirname,"./client/build/index.html"))
+})
 
 app.listen(process.env.PORT, () => {
   console.log(
